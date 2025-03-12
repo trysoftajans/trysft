f14ed10f1e645a1ed3481daa03ff8d7c4f06282e
traysoftajans <traysoftajans@gmail.com>
2025-03-12 18:10:31 +0300

    Yaptığım değişiklikleri açıklayan mesaj

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,11 +1,11 @@
 import { createRequestHandler } from "@remix-run/netlify";
-import * as build from "@remix-run/dev/server-build";
 
-// Doğru build nesnesini kontrol edin ve gerekirse düzeltin
-console.log("Build nesnesi:", build);
+
+
+
 
 export const handler = createRequestHandler({
-  build,
+ 
   mode: process.env.NODE_ENV,
   getLoadContext(event) {
     // Eğer özel bir context gerekiyorsa buraya ekleyin

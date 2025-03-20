d7ba59819dc7aac0452d54bd223b596fd8f2dd2d
traysoftajans <traysoftajans@gmail.com>
2025-03-20 19:10:15 +0300

    Yaptığım değişiklikleri açıklayan mesaj

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,8 +1,12 @@
 import { createRequestHandler } from "@remix-run/netlify";
-import * as remixBuild from "../build/server/index.js";
+import * as remixBuild from "../../build/server/index.js";
 
 export const handler = createRequestHandler({
-  build: remixBuild,
+  build: {
+    ...remixBuild,
+    routes: remixBuild.routes || {},
+    assets: remixBuild.assets || { version: "dev", entry: { module: "./entry.server.tsx" } }
+  },
   mode: process.env.NODE_ENV,
   getLoadContext(event) {
     return {
